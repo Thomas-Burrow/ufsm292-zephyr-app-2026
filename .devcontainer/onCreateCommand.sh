71d9c1aeb4ff0ff9011042017a89da8115d3e6ab
Thomas Patrick Burrow <thomas.burrow@ecomp.ufsm.br>
2026-09-15 16:36:07 -0300

    Devcontainer para o codespace do repositório

---
+++ .devcontainer/onCreateCommand.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+west init -l app
+west update
+west zephyr-export
+west patch apply
+uv pip install -r deps/zephyr/scripts/requirements.txt
+echo "alias ll='ls -lah'" >> $HOME/.bashrc
+echo "alias la='ls -lah'" >> $HOME/.bashrc
+west completion bash > $HOME/west-completion.bash
+echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
+history -c
